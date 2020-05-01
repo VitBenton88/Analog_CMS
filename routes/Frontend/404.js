e77a2041c002b6e877abbfd57148faba5a60162d
Vit Benton <viteksk8alot@gmail.com>
2020-05-01 11:28:37 -0400

    minor code refactor

--- routes/Frontend/404.js
+++ routes/Frontend/404.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
 
 	// Render 404 Page
 	// =============================================================
-	app.all(['/Error404', '404'], async (req, res, next) => {
+	app.all(['/Error404', '404'], (req, res, next) => {
         const { menus, site_data } = req
 
         res.status(404).render('templates/defaults/404', {
